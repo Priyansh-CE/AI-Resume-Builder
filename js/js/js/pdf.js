20d7eebd799c2ee79e9039166b3db4c81d1e9546
Patel Priyansh <patelpriyanshnirmalbhai@gmail.com>
2026-01-28 20:24:09 +0530

    Update pdf.js

--- js/js/js/pdf.js
+++ js/js/js/pdf.js
@@ -1,1 +1,4 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js"></script>
+function downloadPDF() {
+  const element = document.getElementById("resume");
+  html2pdf().from(element).save("Resume.pdf");
+}
