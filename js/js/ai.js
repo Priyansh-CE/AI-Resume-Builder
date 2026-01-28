ff7c99247e71db1fcc393a790000d6b7c964effe
Patel Priyansh <patelpriyanshnirmalbhai@gmail.com>
2026-01-28 20:49:11 +0530

    Delete js/js directory

--- js/js/ai.js
+++
@@ -1,7 +0,0 @@
-function generateAI() {
-  const role = document.getElementById("role").value;
-
-  const aiSummary = `Motivated ${role} with strong problem-solving skills and hands-on project experience. Passionate about building scalable and user-friendly applications.`;
-
-  document.getElementById("r-projects").innerText = aiSummary;
-}
