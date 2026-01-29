4dea83ee124973cfb88c360599a9683c22510763
Patel Priyansh <patelpriyanshnirmalbhai@gmail.com>
2026-01-29 10:08:44 +0530

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD3_1AO7ZmoHYFgiHeLjOJwSbGMGmq06bA",
+  authDomain: "ai-resume-builder-7575c.firebaseapp.com",
+  projectId: "ai-resume-builder-7575c",
+  storageBucket: "ai-resume-builder-7575c.firebasestorage.app",
+  messagingSenderId: "701138705545",
+  appId: "1:701138705545:web:8cfac1243aca3943523a58",
+  measurementId: "G-CXE7QLC8LQ"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebase.firestore();
